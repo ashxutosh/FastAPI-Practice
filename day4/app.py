9a5c7fcf5b66c932012c21b9bde1121e02783062
ashxutosh <ashutoshrout002@gmail.com>
2025-09-28 23:09:05 +0530

    Day-4 after using query  path and HTTPException

--- day4/app.py
+++ day4/app.py
@@ -20,3 +20,20 @@ def patient_id(patient_id: str = Path(...,description="This is the Patient_Id", 
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=400, detail="Bad Request")
+
+@app.get("/sort")
+def sorting(sort_by: str = Query(...,description="sort by bmi,height,weight",),order: str =Query("asc" , description="asc or desc order")):
+     data= patient()
+
+     fields= ["bmi","height","weight"]
+
+     if sort_by not in fields:
+         raise HTTPException(status_code=400, detail="Bad Request")
+     if order not in ["asc","desc"]:
+            raise HTTPException(status_code=400, detail="Bad Request")
+     
+     sort_order= False if order=="desc" else True
+
+     sorted_data= sorted(data.items(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
+
+     return sorted_data
